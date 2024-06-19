71d0e8606929a250f62629b3fb19eb8141fa810c
yasmin-gamal1 <yasmingamal.edu0@gmail.com>
2024-06-19 15:32:35 +0300

    Configure app to use environment port and gunicorn

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -29,4 +30,5 @@ def predict():
         return jsonify({'error': 'Request must be JSON'}), 400
 
 if __name__ == '__main__':
-        app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
